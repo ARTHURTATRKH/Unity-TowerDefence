fdf830b5b055cb748a00942df42bb526b0dff0e1
Arthur Tatarkhan <107375794+ARTHURTATRKH@users.noreply.github.com>
2025-05-12 15:33:59 -0700

    Nodes and Starting code

---
+++ TowerDefence/Assets/Scripts/Tower.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public double range;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
