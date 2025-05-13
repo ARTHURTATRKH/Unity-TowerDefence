8db9ceda44f773496b925c650c3fef4ed49eff24
Arthur Tatarkhan <107375794+ARTHURTATRKH@users.noreply.github.com>
2025-05-12 21:23:39 -0700

    Enemy and Tower updates

--- TowerDefence/Assets/Scripts/Enemy.cs
+++ TowerDefence/Assets/Scripts/Enemy.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int health = 100;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,16 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public int dealDamage(int damage)
+    {
+        health = health - damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            return 0;
+        }
+        return 1;
     }
 }
--- TowerDefence/Assets/Scripts/Tower.cs
+++ TowerDefence/Assets/Scripts/Tower.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    // sets the range of the child gameobject that checks 
     public double range;
+    public int damage = 10;
+    public float gold;
     void Start()
     {
         
@@ -12,5 +15,20 @@ public class Tower : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                if(enemy.dealDamage(damage) == 0)
+                {
+                    gold +=20;
+                }
+            }
+        }
     }
 }
